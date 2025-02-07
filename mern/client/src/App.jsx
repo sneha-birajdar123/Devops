f193b45ed7552f4493b31590c03caac37a2ef8d5
sneha <neha06844@gmail.com>
2025-02-07 16:23:08 +0530

    devOps

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className='text-4xl font-bold text-center m-10 text-blue-700'>Hello this is sneha</h1>
+  )
+}
+
+export default App
